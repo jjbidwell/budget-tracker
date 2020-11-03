a61d28c017a80810823185a31c8eabf747763eb5
Joshua Bidwell <jjbidwell262@gmail.com>
2020-11-02 19:27:39 -0800

    changed name of style folder to css, and made conversions where necessary

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,12 +7,12 @@ const FILES_TO_CACHE = [
   "/index.html",
   "/favicon.ico",
   "/manifest.webmanifest",
-  "/assets/styles/style.css",
+  "/assets/css/style.css",
   "/assets/js/index.js",
   "/assets/images/icons/icon-192x192.png",
   "/assets/images/icons/icon-512x512.png"
 ];
-
+//public\assets\styles\styles.css
 // install
 self.addEventListener("install", function (evt) {
   // pre cache image data
